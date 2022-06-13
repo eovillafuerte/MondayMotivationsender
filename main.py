e5c01f18656c89dce335dc657c130791957c726f
eovillafuerte <eovillafuerte@outlook.com>
2022-06-13 13:58:42 -0600

    imports

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import datetime as dt
+import smtplib
+import random
 
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        
